79769b67adb3099a9566fce6aa6016c7159ef6ff
Justin Ross <rossj190@gmail.com>
2017-08-16 03:56:22 -0700

    Got rotation + following camera sort of working

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -9,27 +9,23 @@ public class FollowCamera : MonoBehaviour {
     public float followHeight = 5f;
     public float moveSpeed = 10f;
     public float rotateSpeed = 10f;
-    public float camRotSpeed = 8f;
+    public float camRotSpeed = 5f;
+    private Vector3 offset;
 
     private Vector3 newPos = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		
+        newPos = new Vector3(target.position.x, target.position.y + followHeight, target.position.z - followDistance);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        newPos.x = target.position.x;
-        newPos.y = target.position.y + followHeight;
-        newPos.z = target.position.z - followDistance;
+	void LateUpdate () {
+        newPos = Quaternion.AngleAxis(Input.GetAxis("Joy X") * camRotSpeed, Vector3.up) * newPos;
+        newPos = Quaternion.AngleAxis(Input.GetAxis("Joy Y") * camRotSpeed, Vector3.left) * newPos;
 
-        //transform.position = Vector3.Lerp(transform.position, newPos, moveSpeed * Time.deltaTime);
-        
-        Vector2 rightStickInput = new Vector2(Input.GetAxis("Joy X"), Input.GetAxis("Joy Y"));
+        transform.position = target.position + newPos;
 
-        transform.LookAt(target);
-        transform.Translate(rightStickInput * Time.deltaTime * camRotSpeed);
-
+        transform.LookAt(target.position);
     }
 }

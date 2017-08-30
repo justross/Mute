4c6a057dd35c0e1f80df16c4b093a630c5f81322
Justin Ross <rossj190@gmail.com>
2017-08-29 19:55:29 -0700

    FollowCamera.cs refactoring

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -54,20 +54,17 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if(Input.GetButton("Cam Center"))
+        if (Input.GetButton("Cam Center"))
         {
             cameraState = CameraState.centering;
         }
-        Debug.Log(transform.rotation);
+
         switch (cameraState)
         {
             case CameraState.centering:
                 transform.forward = target.GetChild(0).forward;
-
-                //newPos.x = Mathf.Lerp(transform.forward.x, target.GetChild(0).forward.x, Time.deltaTime * centeringSpeed);
-                //newPos.z = Mathf.Lerp(transform.forward.z, target.GetChild(0).forward.z, Time.deltaTime * centeringSpeed);
-                //transform.forward = newPos;
-                //if(newPos.x == target.GetChild(0).forward.x && newPos.z == target.GetChild(0).forward.z)
+                rotationXAxis = camera.transform.localRotation.eulerAngles.x;
+                rotationYAxis = transform.localRotation.eulerAngles.y;
                 cameraState = CameraState.idle;
                 break;
 
@@ -78,37 +75,37 @@ public class FollowCamera : MonoBehaviour
                 newPos.y = Mathf.Lerp(newPos.y, target.position.y, Time.deltaTime * verticalFollowSpeed);
                 newPos.z = Mathf.Lerp(newPos.z, target.position.z, Time.deltaTime * followSpeed);
                 transform.position = newPos;
-                
+
                 // update rotation of camera
                 newYaw = Input.GetAxis("Joy Y") * rotateSensitivity * Time.deltaTime;
                 newPitch = Input.GetAxis("Joy X") * rotateSensitivity * Time.deltaTime;
                 velocityY += newYaw;
                 velocityX += newPitch;
-                rotationYAxis += velocityX;
-                rotationXAxis += velocityY;
-                rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
-                Quaternion toRotation = Quaternion.Euler(rotationXAxis, 0, 0);
-                Quaternion toPitch = Quaternion.Euler(0, rotationYAxis, 0);
-                Quaternion pitch = toPitch;
-                Vector3 negDistance = new Vector3(0.0f, followHeight, -followDistance);
-                Vector3 position = toRotation * negDistance;
 
-                camera.transform.localRotation = toRotation;
-                camera.transform.localPosition = position;
-                transform.localRotation = pitch;
-
-                // checks if a wall is between where the camera moved and the player
-                RaycastHit wallHit = new RaycastHit();
-                if (Physics.Linecast(target.position, camera.transform.position, out wallHit, camOcclusion))
-                {
-                    Vector3 absPosition = new Vector3(wallHit.point.x + wallHit.normal.x, wallHit.point.y + wallHit.normal.y, wallHit.point.z + wallHit.normal.z);
-                    camera.transform.position = absPosition;
-                }
-
-                velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * rotateDamping);
-                velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * rotateDamping);
                 break;
         }
+
+        rotationYAxis += velocityX;
+        rotationXAxis += velocityY;
+        rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
+        Quaternion rotation = Quaternion.Euler(rotationXAxis, 0, 0);
+        Quaternion pitch = Quaternion.Euler(0, rotationYAxis, 0);
+        Vector3 negDistance = new Vector3(0.0f, followHeight, -followDistance);
+        Vector3 position = rotation * negDistance;
+
+        camera.transform.localRotation = rotation;
+        camera.transform.localPosition = position;
+        transform.localRotation = pitch;
+
+        // checks if a wall is between where the camera moved and the player
+        RaycastHit wallHit = new RaycastHit();
+        if (Physics.Linecast(target.position, camera.transform.position, out wallHit, camOcclusion))
+        {
+            Vector3 absPosition = new Vector3(wallHit.point.x + wallHit.normal.x, wallHit.point.y + wallHit.normal.y, wallHit.point.z + wallHit.normal.z);
+            camera.transform.position = absPosition;
+        }
+        velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * rotateDamping);
+        velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * rotateDamping);
 
 
     }

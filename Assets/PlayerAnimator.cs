7819019df349bf01ea025152cf435a69700ce6a8
Justin Ross <rossj190@gmail.com>
2017-08-31 17:36:37 -0700

    Added sanity check to prevent console spam

--- Assets/PlayerAnimator.cs
+++ Assets/PlayerAnimator.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour {
+public class PlayerAnimator : MonoBehaviour
+{
 
     private PlayerMotor playerMotor;
     public float rotationDamping = 7.5f;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerMotor = transform.parent.GetComponent<PlayerMotor>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         direction = playerMotor.cameraRig.TransformDirection(direction);
-        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-        if (direction.magnitude > 0.1f)
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationDamping * Time.deltaTime);
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+            if (direction.magnitude > 0.1f)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationDamping * Time.deltaTime);
+            }
+        }
 
     }
 }
